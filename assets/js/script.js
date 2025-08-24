2ab799329e988bfa0134df1d9079c73ba8b60b46
joni <codingmylife0@gmail.com>
2025-08-24 17:41:55 +0530

    lenis animation add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-gsap.registerPlugin(SplitText)
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 let heading = SplitText.create(".heading", { type: "words, chars" });
 
@@ -9,12 +10,92 @@ gsap.from(heading.chars, {
     stagger: 0.05
 });
 
-let subHeading = SplitText.create(".subHeading", { type: "lines, words", });
+let subHeading = SplitText.create(".subHeading", { type: " words", });
 
-// now animate the characters in a staggered fashion
-gsap.from(subHeading.lines, {
+
+gsap.from(subHeading.words, {
     duration: 1,
-    y: 100,       // animate from 100px below
-    autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
-    stagger: 0.05 // 0.05 seconds between each
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05
 });
+
+let leistungenSection = SplitText.create(".leistungen-section h2", { type: "chars", });
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".leistungen-section",
+        // markers: true,
+        scroller: "body",
+        start: "top 90%",
+        end: "bottom 70%",
+        scrub: 1
+    }
+});
+
+tl.from(leistungenSection.chars, {
+    duration: 0.6,
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05
+});
+
+
+
+gsap.set(".leistungen-card",
+    {
+        scaleY: 0,
+        opacity: 0,
+        transformOrigin: "bottom"
+    });
+
+tl.to(".leistungen-card", {
+    scaleY: 1,
+    opacity: 0.7,
+    duration: 1.2,
+    ease: "power2.out",
+    stagger: 0.5
+});
+
+
+let darumlumusSection = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".darumlumus-section",
+        // markers: true,
+        scroller: "body",
+        start: "top 90%",
+        end: "bottom 70%",
+        scrub: 1
+    }
+});
+
+let darumlumusHeading = SplitText.create(".darumlumus-section h2", { type: "chars", });
+
+darumlumusSection.from(darumlumusHeading.chars, {
+    duration: 0.6,
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05
+});
+
+darumlumusSection.to(".darumlumus-btn", {
+    opacity: 0.7,
+    duration: 1.2,
+    ease: "power2.out",
+})
+
+
+
+const lenis = new Lenis();
+
+// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+lenis.on('scroll', ScrollTrigger.update);
+
+// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+// This ensures Lenis's smooth scroll animation updates on each GSAP tick
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+});
+
+// Disable lag smoothing in GSAP to prevent any delay in scroll animations
+gsap.ticker.lagSmoothing(0);
