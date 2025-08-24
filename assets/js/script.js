0669d09415410236d1b99af3416939b7ec76461a
joni <codingmylife0@gmail.com>
2025-08-25 00:42:11 +0530

    parther section add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 
+
+const lenis = new Lenis();
+lenis.on('scroll', ScrollTrigger.update);
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+});
+gsap.ticker.lagSmoothing(0);
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 let heading = SplitText.create(".heading", { type: "words, chars" });
@@ -11,7 +19,6 @@ gsap.from(heading.chars, {
 });
 
 let subHeading = SplitText.create(".subHeading", { type: " words", });
-
 
 gsap.from(subHeading.words, {
     duration: 1,
@@ -40,8 +47,6 @@ tl.from(leistungenSection.chars, {
     stagger: 0.05
 });
 
-
-
 gsap.set(".leistungen-card",
     {
         scaleY: 0,
@@ -56,7 +61,6 @@ tl.to(".leistungen-card", {
     ease: "power2.out",
     stagger: 0.5
 });
-
 
 let darumlumusSection = gsap.timeline({
     scrollTrigger: {
@@ -84,18 +88,22 @@ darumlumusSection.to(".darumlumus-btn", {
     ease: "power2.out",
 })
 
-
-
-const lenis = new Lenis();
-
-// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
-lenis.on('scroll', ScrollTrigger.update);
-
-// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
-// This ensures Lenis's smooth scroll animation updates on each GSAP tick
-gsap.ticker.add((time) => {
-    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+let feferenzensection = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".feferenzen-section",
+        // markers: true,
+        scroller: "body",
+        start: "top 90%",
+        end: "bottom 70%",
+        scrub: 1
+    }
 });
 
-// Disable lag smoothing in GSAP to prevent any delay in scroll animations
-gsap.ticker.lagSmoothing(0);
+let feferenzensectionHeading = SplitText.create(".feferenzen-heading", { type: "chars", });
+
+feferenzensection.from(feferenzensectionHeading.chars, {
+    duration: 0.6,
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05
+});
